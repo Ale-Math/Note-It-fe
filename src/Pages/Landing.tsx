35d6292aa7d19b5b4fa6ae4ee51edb7e6a08a8c0
Alexander Mathew <alexandermathew27@gmail.com>
2025-04-02 05:08:03 +0530

    added next div

--- src/Pages/Landing.tsx
+++ src/Pages/Landing.tsx
@@ -1,44 +1,79 @@
-import { Android } from "../assets/Android"
-import { Apple } from "../assets/Apple"
-import { Logo } from "../Components/Icons/Logo"
-import { Button } from "../Components/UI/Button"
+import { Android } from "../assets/Android";
+import { Apple } from "../assets/Apple";
+import { Logo } from "../Components/Icons/Logo";
+import { Button } from "../Components/UI/Button";
 
 export function Landing() {
-    return <div className="w-screen h-screen overflow-x-hidden bg-stone-50"> 
-    <div className="flex justify-between p-5 items-center w-screen sticky -top-0 bg-stone-50 mb-24">
+  return (
+    <div className="w-screen h-screen overflow-x-hidden bg-stone-50">
+      <div className="flex justify-between p-5 items-center w-screen sticky -top-0 bg-stone-50 mb-24">
         <a href="/">
-        <Logo/>
+          <Logo />
         </a>
         <div>
-        <Button variant="secondary" size="lg" text="Log in" onClick={()=>{}}></Button>
-        <Button variant="primary" size="lg" text="Start for free" onClick={()=>{}}></Button>
+          <Button
+            variant="secondary"
+            size="lg"
+            text="Log in"
+            onClick={() => {}}
+          ></Button>
+          <Button
+            variant="primary"
+            size="lg"
+            text="Start for free"
+            onClick={() => {}}
+          ></Button>
         </div>
+      </div>
+      <div className="flex justify-center">
+        <div className="flex justify-between p-5 pl-24 space-x-5">
+          <div className="space-y-5 w-1/3">
+            <p className="text-5xl font-mono font-bold pb-3 ">
+              Organize your work and life,finally.
+            </p>
+            <p className="text-2xl text-gray-600 font-sans">
+              Simplify life for both you and your team with the world's #1 task
+              manager and to-do list app.
+            </p>
+            <div className="flex items-center">
+              <p className="text-gray-600 font-semibold font-mono">
+                500K+ ★★★★★ reviews from{" "}
+              </p>
+              <div className="flex items-center">
+                <Apple></Apple>
+                <Android></Android>
+              </div>
+            </div>
+            <div className="pt-5">
+              <Button
+                variant="primary"
+                size="lg"
+                text="Start for free"
+                onClick={() => {}}
+              ></Button>
+            </div>
+          </div>
+          <div className="w-2/3 pl-2">
+            <img src="/appbg.avif" className=" bg-red-100 rounded-2xl"></img>
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-center bg-[url(/bg-image.avif)] bg-contain">
+        <div className=" flex justify-around p-20 text-lg font-mono w-5/6">
+          <div className=" flex-col justify-center">
+            <h1>“Simple, straightforward, and super powerful”</h1>
+            <img src="/verge.png" className="w-15 h-5"></img>
+          </div>
+          <div className="flex-col justify-center">
+            <h1>“The best to-do list app on the market”</h1>
+            <img src="/verge.png" className="w-15 h-5"></img>
+          </div>
+          <div className="flex-col justify-center">
+            <h1>“Nothing short of stellar”</h1>
+            <img src="/verge.png" className="w-15 h-5"></img>
+          </div>
+        </div>
+      </div>
     </div>
-    <div className="flex justify-center">
-    <div className="flex justify-between p-5 pl-24 space-x-10">
-        <div className="space-y-5 w-2/5">
-        <p className="text-6xl font-mono font-bold ">Organize your work and life,finally.</p>
-        <p className="text-2xl text-gray-600 font-mono">Simplify life for both you and your team with the world's #1 task manager and to-do list app.</p>
-        <div className="flex items-center">
-        <p className="text-gray-600 font-semibold font-mono">500K+ ★★★★★ reviews from </p>
-        <div className="flex items-center">
-        <Apple></Apple>
-        <Android></Android>
-        </div>
-        </div>
-        <div className="pt-5">
-        <Button variant="primary" size="lg" text="Start for free" onClick={()=>{}}></Button>
-        </div>
-        </div>
-        <div className="w-3/5 pl-2">
-            <img src="/appbg.avif" className=" bg-red-100 rounded-lg"></img>
-        </div>
-    </div>
-    </div>
-    <div className="bg-[url('/Noteit.png')]">
-        <div></div>
-        <div></div>
-        <div></div>
-    </div>
-    </div>
+  );
 }
