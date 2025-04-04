2d40e00e62842ba504a0b62186f209c0289a51a3
Alexander Mathew <alexandermathew27@gmail.com>
2025-04-04 21:55:44 +0530

    added card component

--- src/Components/UI/Button.tsx
+++ src/Components/UI/Button.tsx
@@ -11,9 +11,9 @@ const variantStyles = {
   secondary:
     "bg-stone-50 text-black rounded-md px-2 hover:bg-gray-200 font-mono",
   tertiary:
-    "bg-gray-100 text-black rounded-md hover:bg-gray-300 font-mono active:bg-color-green-300 font-semibold text-md",
+    "bg-gray-100 text-black rounded-md hover:bg-gray-300 font-mono focus:bg-teal-100 font-semibold text-md",
   shadow:
-    "bg-orange-500 text-white rounded-2xl font-bold font-mono border-b-8 border-orange-300 transition smooth duration-150ms ease-in-out  hover:border-b-4 hover:translate-y-1 active:border-b-0 active:translate-y-2 ",
+    "bg-orange-500 text-white rounded-2xl font-bold font-mono border-b-8 border-orange-300 duration-300 ease-in-out  hover:border-b-4 hover:translate-y-1 active:border-b-0 active:translate-y-2 ",
 };
 
 const sizeStyles = {
