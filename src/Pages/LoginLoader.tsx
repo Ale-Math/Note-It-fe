38598c98a401c11ae5eeec02fd063e53a1b7436e
Alexander Mathew <alexandermathew27@gmail.com>
2025-05-19 04:47:07 +0530

    added original logos

--- src/Pages/LoginLoader.tsx
+++ src/Pages/LoginLoader.tsx
@@ -15,7 +15,7 @@ export function LoginLoader() {
       <div>
         <img
           className="lg:max-w-24 lg:max-h-24 lg:pb-4 md:max-w-20 md:max-h-20 md:pb-3 sm:pb-2 sm:max-w-12 sm:max-h-12 max-w-8 max-h-8"
-          src="\Noteit.png"
+          src="\ColorNoteit!.png"
         />
         <Spinner></Spinner>
       </div>
