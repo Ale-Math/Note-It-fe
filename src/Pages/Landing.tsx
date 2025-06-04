f0bef44ef9c35772e9489afdcf2c43a10c4d3595
Alexander Mathew <alexandermathew27@gmail.com>
2025-06-04 18:55:01 +0530

    update video

--- src/Pages/Landing.tsx
+++ src/Pages/Landing.tsx
@@ -129,14 +129,14 @@ export function Landing() {
           <div className=" md:w-1/2 w-full space-y-28 ">
             {!scrolled ? (
               <div className="sticky top-48">
-                <div className="w-full md:p-10 p-5 bg-red-50 shadow rounded-2xl bg-gradient-to-br from-green-800 via-red-300 to-red-200">
+                <div className="w-full md:p-5 p-5 bg-red-50 shadow rounded-2xl bg-gradient-to-br from-green-800 via-red-300 to-red-200">
                   <video
                     ref={videoRef}
-                    src="/AddTaskVideo.mp4"
+                    src="/AddTask.mp4"
                     autoPlay
                     loop
                     muted
-                    className="rounded-md hover:scale-110 transition duration-700"
+                    className="rounded-md hover:scale-105 transition duration-700"
                   />
                 </div>
               </div>
@@ -144,7 +144,7 @@ export function Landing() {
               <div className="sticky top-48">
                 <div className="w-full md:p-10 p-5 bg-red-50 shadow rounded-2xl bg-gradient-to-br from-cyan-800 via-red-300 to-red-200">
                   <video
-                    src="/AddTaskVideo.mp4"
+                    src="/AddTask.mp4"
                     autoPlay
                     loop
                     muted
